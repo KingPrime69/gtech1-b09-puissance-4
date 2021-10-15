789040f6cd6a4e550d9e915e385ab46e38502922
KingPrime69 <quentin.ripot@free.fr>
2021-10-15 23:04:34 +0200

    grille en cours

--- script.c
+++ script.c
@@ -1,1 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define NBL 6
+#define NBC 7
+int line;
+char tab[NBL][NBC];
+int count;
+char i;
+char u;
+void init(void) {
+  for(int l=0; l<NBL; l++) {
+    for(int c=0; c<NBC; c++) {
+    }
+  }
+}
+
+void main(void){
+  init();
+  while (line<=7){
+      tab[i][u] = '|';
+      printf("%s", *tab);
+      tab[i][u] = '.';
+      printf("%s", *tab);
+      printf("\n");
+  }
+}
+	
