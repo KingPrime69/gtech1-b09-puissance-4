42b15bed84653c8a15d64cdeabe08d9210fea8cc
KingPrime69 <quentin.ripot@free.fr>
2021-10-17 22:04:18 +0200

    +Menu

--- script.c
+++ script.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include<string.h>
 
 #define NBL 6
 #define NBC 7
 
+char request[10] = "wait";
+char start[10] = "jouer";
+char norm[10] = "regles";
 char tab[NBL][NBC];
 char i;
 char u;
 int count;
+
 void init(void) {
   for(count=0; count<15; count++)
     printf("_");
@@ -34,7 +39,43 @@ void init(void) {
   printf("|");
   printf("\n");
 }
+
+void menu(void){
+  for(count=0; count<15; count++)
+    printf("_");
+  printf("\n\n");
+  printf("     -MENU\n");
+  printf("-Régles du jeu\n");
+  printf("-Lancer la partie\n");
+  for(count=0; count<15; count++)
+    printf("_");
+  printf("\n\n\n");
+  printf("Choissisez un sommaire\n");
+  printf("-jouer (lancera la partie)\n");
+  printf("-régles (affichera les régles et commandes du jeu)\n");
+}
+
+void rule(void){
+  for(count=0; count<15; count++)
+    printf("_");
+  printf("\n\n");
+  printf("Règles du jeu\n");
+  for(count=0; count<15; count++)
+    printf("_");
+  printf("\n\n");
+}
+
 void main(void){
-  init();
+  menu();
+  scanf("%10s", request);
+  if(strcmp(request, start) == 0){
+    init();
+  }
+  else if(strcmp(request, norm) == 0){
+    rule();
+  }
+  else{
+    printf("erreur");
+  }
 }
 	
