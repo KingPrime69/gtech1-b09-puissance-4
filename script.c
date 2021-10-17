15dfd95b8283a6c121afc8af8c13eb0f3837554c
KingPrime69 <quentin.ripot@free.fr>
2021-10-17 13:02:48 +0200

    affichage de la grille puissance-4

--- script.c
+++ script.c
@@ -3,26 +3,38 @@
 
 #define NBL 6
 #define NBC 7
-int line;
+
 char tab[NBL][NBC];
-int count;
 char i;
 char u;
+int count;
 void init(void) {
+  for(count=0; count<15; count++)
+    printf("_");
+  printf("\n\n");
+  printf("  PUISSANCE 4\n");
+  for(count=0; count<15; count++)
+    printf("_");
+  printf("\n\n");
   for(int l=0; l<NBL; l++) {
     for(int c=0; c<NBC; c++) {
-    }
-  }
-}
-
-void main(void){
-  init();
-  while (line<=7){
       tab[i][u] = '|';
-      printf("%s", *tab);
+      printf("%s",*tab);
       tab[i][u] = '.';
       printf("%s", *tab);
-      printf("\n");
+    }
+    tab[i][u] = '|';
+    printf("%s", *tab);
+    printf("\n");
   }
+  for(count=1; count != 8; count++){
+    printf("|");
+    printf("%d", count);
+  }
+  printf("|");
+  printf("\n");
+}
+void main(void){
+  init();
 }
 	
