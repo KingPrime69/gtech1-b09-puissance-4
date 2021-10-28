3ca756f08931d9369d3351798afe559ec133780b
KingPrime69 <92248230+KingPrime69@users.noreply.github.com>
2021-10-28 21:10:42 +0200

    affichage tab dyna + no bug dans nav in game

--- script.c
+++ script.c
@@ -1,5 +1,4 @@
 //rajouter les commandes et mettre la navigation texte pendant le jeux
-//essaye avec lecture de hit en %d et mettre quand 0 passer en mode txt
 #include <stdio.h>
 #include <stdlib.h>
 #include<string.h>
@@ -7,6 +6,9 @@
 #define NBL 100
 #define NBC 100
 
+int login = 1;
+int tab[NBL][NBC];
+int add;
 int loop;
 int line;
 int slide[NBC];
@@ -18,13 +20,12 @@ char request[10] = "wait";
 char start[10] = "jouer";
 char norm[10] = "regles";
 char head[5] = "menu";
-char tab[NBL][NBC];
 int count;
 int player = 0;
 int* L;
 int* C;
-int add;
-
+char resume[10] = "reprendre";
+char restart[10] = "rejouer";
 
 void title(char* heading){
   for(count=0; count<15; count++)
@@ -37,22 +38,8 @@ void title(char* heading){
   printf("\n\n");
 }
 
-void grill(void) {
-  C = malloc(sizeof(int));
-  printf("nombre de colone ?\n");
-  scanf("%d", C);
-  L = malloc(sizeof(int));
-  printf("nombre de ligne ?\n");
-  scanf("%d", L);
-  for(int l=0; l<*L; l++) {
-    for(int c=0; c<*C; c++) {
-      tab[l][c] = '.';
-    }
-  }
-}
-
 void show_grill(void){
-  title("  Puissance 4");
+  title("  Pussance 4");
   for(int l=0; l<*L; l++){
     printf("\n");
     for(int c=0; c<*C; c++) {
@@ -66,6 +53,63 @@ void show_grill(void){
   printf("|\n");
 }
 
+void show_grill_D(void){
+  title("  Pussance 4");
+  for(int l=0; l<*L; l++){
+    printf("\n");
+    for(int c=0; c<*C; c++) {
+      if(c == 0)
+        printf("| %c", tab[l][c]);
+      else
+        printf(" | %c", tab[l][c]);
+    }
+    printf(" |");
+  }
+  printf("\n");
+  for(int c=1; c<*C+1; c++){
+    if(c>10)
+      printf("| %d", c);
+    else if(c == 1)
+      printf("| %d", c);
+    else
+      printf(" | %d", c);
+  }
+  printf("|\n");
+}
+
+void grill(void){
+  while(login == 1){
+    C = malloc(sizeof(int));
+    printf("nombre de colone ?\n");
+    scanf("%d", C);
+    L = malloc(sizeof(int));
+    printf("nombre de ligne ?\n");
+    scanf("%d", L);
+    if(*C == 0 || *L == 0){
+      printf("Veuillez saisir un nombre au dessu de 0\n");
+      while((*C = getchar()) != '\n' && *C != EOF && (*L = getchar()) != '\n' && *L != EOF);
+      free(C);
+      free(L);
+      login = 1;
+    }
+    else{
+      for(int l=0; l<*L; l++) {
+        for(int c=0; c<*C; c++) {
+          tab[l][c] = '.';
+        }
+      }
+      for (int l=0; l<*L; l++){
+        slide[l] = 0;
+      }
+      if(*C>=10)
+        show_grill_D();
+      else
+        show_grill();
+      login = 0;
+    }
+  }
+}
+
 void menu(void){
   for(count=0; count<15; count++)
     title("    -Menu");
@@ -77,7 +121,7 @@ void menu(void){
 void rule(void){
   title(" Règles du jeux");
   printf("Les règles du puissance 4 sont simple le but est d'être le premiers à aligner 4 jetons horizontalement, ve\
-\rticalement ou diagonalement.\n");
+rticalement ou diagonalement.\n");
   title("   Commandes");
   printf("écrire les comandes pour naviguer dans le jeux\n");
 }
@@ -88,31 +132,56 @@ void player_stroke(int playerAction, int next){
   slide[playerAction]++;
 }
 
-void pawn(){
-  for (int l=0; l<line; l++){
-    slide[l] = 0;
-  }
-  printf("\n");
-}
-
 void game(){
   grill();
-  show_grill();
   loop = 1;
   while(loop == 1){
-    hit = 0;
-    pawn();
-    printf("Ou voulez vous placez votre pion?\n");
-    scanf("%s", &hit);
-    if(hit>48 && hit <=*C+48){
-      hit =hit-48;
+    while((hit = getchar()) != '\n' && hit != EOF);
+    printf("\nOu voulez vous placez votre pion?\n");
+    scanf("%d", &hit);
+    if(hit == 0){
+      init = 1;
+      while(init == 1){
+        printf("vous êtes en mode commande\n");
+        scanf("%s", request);
+        if(strcmp(request, restart) == 0){
+          free(L);
+          free(C);
+          login = 1;
+          game();
+        }
+        else if(strcmp(request, norm) == 0){
+          rule();
+        }
+        else if(strcmp(request, head) == 0){
+          menu();
+          loop = 0;
+        }
+        else if(strcmp(request, resume) == 0){
+          show_grill();
+          init = 0;
+        }
+        else if(strcmp(request, end) == 0){
+          init = 0;
+          free(L);
+          free(C);
+          loop = 0;
+        }
+        else{
+          printf("\ntapez une commande valide !\n");
+        }
+      }
+    }
+    else if(hit>=1 && hit <=*C){
       player_stroke(hit-1, slide[hit-1]);
-      show_grill();
+      if(*C>=10)
+        show_grill_D();
+      else
+        show_grill();
       player = !player;
     }
-    else{
-      printf("Seul les chiffres de 1 à 7 sont acceptable");
-    }
+  else
+    printf("Seul les chiffres de 1 à %d sont acceptable", *C);
   }
 }
 
